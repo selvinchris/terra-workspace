0f09de9b8d9b7e7b2c8d37790a6e3802c4badeb0
Selvin J <selvin.jayaraj@dxc.com>
2025-06-27 16:21:13 +0530

    added output field

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
 provider "aws" {
-
-    region = "us-east-1"
-  
+  region = "us-east-1"
 }
-
-resource "aws_instance" "test1" {
-
-    ami = "ami-00a929b66ed6e0de6"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name= "testing_instance"
-    }
-  
-}
-
-output "instance_id" {
-
-    value= aws_instance.test1.id
-} 
-output "public_ip" {
-
-    value= aws_instance.test1.public_ip
-} 
-output "instance_name" {
-
-    value= aws_instance.test1.tags["Name"]
-} 
